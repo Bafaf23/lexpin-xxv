3f904101f97d9bbc80a563e86867f3bf6e3cdbda
Angélica Rivas <57818532+Angi-1401@users.noreply.github.com>
2025-11-21 22:06:21 -0400

    Add JS Content: Classes

--- js/js/classes.js
+++ js/js/classes.js
@@ -116,3 +116,168 @@ gatito.sleep();
  * - Administrator: department, manage() (método que imprima un
  * mensaje)
  */
+
+class Circle {
+  constructor(radio) {
+    this.radio = radio;
+  }
+
+  area() {
+    return Math.PI * Math.pow(this.radio, 2);
+  }
+}
+
+let circle = new Circle(20);
+
+console.log(circle.area());
+
+class Human {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  introduce() {
+    console.log(`Hi, my name is ${this.firstName} ${this.lastName}`);
+  }
+}
+
+class Administrator extends Human {
+  constructor(firstName, lastName, age, department) {
+    super(firstName, lastName, age);
+    this.department = department;
+  }
+
+  manage() {
+    console.log(
+      `${this.firstName} ${this.lastName} is managing very important stuff.`
+    );
+  }
+}
+
+class Teacher extends Human {
+  constructor(firstName, lastName, age, subject, salary) {
+    super(firstName, lastName, age);
+    this.subject = subject;
+    this.salary = salary;
+  }
+
+  teach() {
+    console.log(
+      `${this.firstName} ${this.lastName} is teaching students about ${this.subject}.`
+    );
+  }
+}
+
+class Student extends Human {
+  constructor(firstName, lastName, age, grade, enrollment = []) {
+    super(firstName, lastName, age);
+    this.grade = grade;
+    this.enrollment = enrollment;
+  }
+
+  study() {
+    console.log(
+      `${this.firstName} ${this.lastName} is studying about ${
+        this.enrollment[Math.floor(Math.random() * this.enrollment.length)]
+      }!`
+    );
+  }
+}
+
+let administrator = new Administrator("Luis", "Gozzo", 30, "HR");
+let teacher = new Teacher("Neymar", "Brito", 28, "Biology", 200);
+let student = new Student("Mauricio", "López", 13, "8th Grade", [
+  "Math",
+  "English",
+  "History",
+]);
+
+administrator.introduce();
+administrator.manage();
+
+teacher.introduce();
+teacher.teach();
+
+student.introduce();
+student.study();
+
+/**
+ * Ejercicio Estelar
+ *
+ * Crear una clase llamada TaskManager que permita gestionar una
+ * lista de tareas. La clase debe tener los siguientes métodos:
+ * - add(task): Agrega una nueva tarea a la lista.
+ * - remove(task): Elimina una tarea de la lista.
+ * - get(): Devuelve todas las tareas en la lista.
+ * - markAsCompleted(task): Marca una tarea como completada.
+ *
+ * Cada tarea debe ser un objeto (clase Task) con las siguientes
+ * propiedades:
+ * - title: Título de la tarea.
+ * - completed: Estado de la tarea (completada o no).
+ */
+
+class TaskManager {
+  constructor() {
+    this.list = [];
+  }
+
+  add(task) {
+    this.list.push(task);
+    console.log(`${task.title} has been added to the list successfully.`);
+  }
+
+  remove(task) {
+    const index = this.list.indexOf(task);
+
+    if (index === -1) console.log("Task not found.");
+
+    this.list.splice(index, 1);
+    console.log(`${task.title} was removed successfully.`);
+  }
+
+  get() {
+    let count = 0;
+
+    for (let element of this.list) {
+      let status;
+      if (element.completed) status = "Completed";
+      else status = "Incompleted";
+
+      console.log(`Task No. ${count}`);
+      console.log(`${element.title} (Status: ${status})\n`);
+
+      count++;
+    }
+  }
+
+  markAsCompleted(task) {
+    task.completed = !task.completed;
+    console.log(`Status of ${task.title} was changed successfully.`);
+  }
+}
+
+class Task {
+  constructor(title) {
+    this.title = title;
+    this.completed = false;
+  }
+}
+
+let task1 = new Task("Pasear al perro");
+let task2 = new Task("Regar las plantas");
+
+let manager = new TaskManager();
+
+manager.add(task1);
+manager.add(task2);
+
+manager.markAsCompleted(task2);
+
+manager.get();
+
+manager.remove(task1);
+
+manager.get();
